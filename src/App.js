7c46eedaf3dd589dd58ba8cd35d8c79f0930072d
SCRISTHIAN <quispeninacristhianlucio@gmail.com>
2023-03-17 07:33:17 +0100

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import { TodoItem } from "./srcs/TodoItem";
+import { TodoList } from "./srcs/TodoList";
+import "./App.css";
+
+const todos = [
+  { text: "PRACTICAR", completed: false },
+  { text: "AJUSTAR CUENTAS", completed: false },
+  { text: "CELEBRAR", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="BoxFather4"></div>
+      <div className="BoxFather">
+        <h1 className="h1sc">Crea Una Nueva Tarea</h1>
+        <h2 className="h2sc">Nombre de tarea</h2>
+        <input className="inputxd" type="text" placeholder="¿Algo mas?"></input>
+      </div>
+      <button className="CreateTodoButton">+</button>
+      <img
+        width="300"
+        height="300"
+        className="UFA"
+        src="https://wonder-day.com/wp-content/uploads/2020/10/wonder-day-among-us-png-42.png"
+      ></img>
+      <div className="BoxFather1"></div>
+      <div className="BoxFather2">
+        <h1 className="h1sc1">TUS TAREAS</h1>
+        <h2 className="h2sc1">3 de 5 Completas</h2>
+        <input
+          className="inputxd1"
+          type="text"
+          placeholder="Busquemos..."
+        ></input>
+      </div>
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <div className="ufasaurio"></div>
+    </React.Fragment>
   );
 }
 
---
+++ src/srcs/Box.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Box(props) {
+  return;
+}
+
+export { Box };
